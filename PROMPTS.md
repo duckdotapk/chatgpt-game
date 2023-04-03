d9c7ebd1aabede5d07292d322aed87655fb559f6
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-04-03 16:56:10 -0400

    Version 2

--- PROMPTS.md
+++ PROMPTS.md
@@ -1,2 +1,5 @@
 ## Prompt 1
 Please write a basic browser based game using JavaScript.
+
+## Prompt 2
+Can you please add an optional upgrade that you can spend points on to auto generate a point every second?
--- src/script.js
+++ src/script.js
@@ -1,8 +1,28 @@
 const clickBtn = document.getElementById('clickBtn');
+const upgradeBtn = document.getElementById('upgradeBtn');
 const scoreElement = document.getElementById('score');
+const upgradeCostElement = document.getElementById('upgradeCost');
 let score = 0;
+let upgradeCost = 10;
 
 clickBtn.addEventListener('click', () => {
     score++;
     scoreElement.textContent = score;
 });
+
+upgradeBtn.addEventListener('click', () => {
+    if (score >= upgradeCost) {
+        score -= upgradeCost;
+        upgradeCost *= 2;
+        scoreElement.textContent = score;
+        upgradeCostElement.textContent = upgradeCost;
+        startAutoClicker();
+    }
+});
+
+function startAutoClicker() {
+    setInterval(() => {
+        score++;
+        scoreElement.textContent = score;
+    }, 1000);
+}
